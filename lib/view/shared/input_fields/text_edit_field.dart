ce7b4c0ac224f98c7d792ec388c17dc213d0561a
sasha <tanksasha77@gmail.com>
2024-08-09 22:21:42 +0300

    feat: add `TextEditController` and `TextEditField` for enhanced text field management
    - Implement `TextEditController` to manage text editing with error handling, text manipulation, and focus management.
    - Create `TextEditField` widget to use `TextEditController` with customizable properties including label, hint, and input styles.
    
    These additions simplify text field handling and validation, improving code organization and reusability in forms and user input scenarios.

---
+++ lib/view/shared/input_fields/text_edit_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:open_work_flutter/view/shared/input_fields/text_edit_controller.dart';
+
+class TextEditField extends StatelessWidget {
+  final TextEditController controller;
+
+  final void Function(String value)? onChanged;
+  final void Function(String value)? onSubmit;
+
+  final String? label;
+  final String? hint;
+
+  final InputBorder? border;
+
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final int? maxLines;
+
+  const TextEditField({
+    super.key,
+    required this.controller,
+    this.onChanged,
+    this.onSubmit,
+    this.label,
+    this.hint,
+    this.border,
+    this.keyboardType,
+    this.textInputAction,
+    this.maxLines,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller.controller,
+      focusNode: controller.focusNode,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      maxLines: maxLines,
+      onChanged: onChanged,
+      onSubmitted: onSubmit,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        errorText: controller.error,
+        border: border,
+      ),
+    );
+  }
+}
