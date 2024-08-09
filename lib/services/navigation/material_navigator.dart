508cce99f145d87f0811015c4d070ee7d66e937d
sasha <tanksasha77@gmail.com>
2024-08-09 21:22:42 +0300

    feat: add `MaterialNavigator` for simplified navigation and result handling
    - `push<T>`: Navigates to a new screen and retrieves a result of type `T`.
    - `pop<T>`: Pops the current screen and optionally returns an empty result.
    - `popWith<T>`: Pops the current screen and returns a specified result.
    - `bottomSheet<T>`: Displays a modal bottom sheet and retrieves a result of type `T`.
    - `isNot<T>`: Validates the type of the returned result and logs an error if it doesn't match `T`.
    
    Integrates with `GetIt` for logging and ensures type safety in navigation results.

---
+++ lib/services/navigation/material_navigator.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
+import 'package:open_work_flutter/services/result.dart';
+
+abstract class MaterialNavigator {
+  static final _logger = GetIt.I.get<Logger>();
+
+  MaterialNavigator._();
+
+  static Future<Result> push<T extends Object?>(
+    BuildContext context,
+    WidgetBuilder builder,
+  ) async {
+    Result? result = await Navigator.push<Result>(
+      context,
+      MaterialPageRoute(builder: builder),
+    );
+
+    if (isNot<T>(result)) {
+      return Result.empty();
+    }
+
+    return result as Result;
+  }
+
+  static void pop<T>(BuildContext context, {int times = 1}) {
+    for (var i = 0; i < times; i++) {
+      Navigator.pop(context, Result.empty());
+    }
+  }
+
+  static void popWith<T>(BuildContext context, Result result) {
+    Navigator.pop(context, result);
+  }
+
+  static Future<Result> bottomSheet<T>({
+    required BuildContext context,
+    required WidgetBuilder builder,
+    bool isScrollControlled = false,
+    bool useSafeArea = false,
+  }) async {
+    final result = await showModalBottomSheet(
+      context: context,
+      builder: builder,
+      isScrollControlled: isScrollControlled,
+      enableDrag: false,
+      useSafeArea: useSafeArea,
+    );
+
+    if (isNot<T>(result)) {
+      return Result.empty();
+    }
+
+    return result as Result;
+  }
+
+  static bool isNot<T>(dynamic object) {
+    if (object is Result) {
+      return false;
+    }
+
+    String message =
+        'Page return $object insead of PageResult<${T.runtimeType}> type';
+
+    _logger.e(message);
+
+    return true;
+  }
+}
