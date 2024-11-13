3831e4b79b22d4b8f712560199b0aed3c022b956
sasha <tanksasha77@gmail.com>
2024-11-13 14:41:10 +0200

    feat: add methods to check and update WorkMonth by date in WorkMonthSembastStorage
    - Implemented `containsMonth` to check if a `WorkMonth` exists for a given `DateTime`.
    - Added `updateOrCreateByDate` to update an existing `WorkMonth` or create a new one based on the date.
    - Introduced helper method `getIdByDate` to retrieve the ID of a `WorkMonth` based on its date.
    - Created `regexFromYearMonth` to generate a regular expression for matching `WorkMonth` records by year and month.

--- lib/storage/month_storage.dart
+++ lib/storage/month_storage.dart
@@ -1,4 +1,7 @@
 import 'package:open_work_flutter/data/models/work_month.dart';
 import 'package:open_work_flutter/storage/entity_storage_async.dart';
 
-abstract class MonthStorage extends EntityStorageAsync<int, WorkMonth> {}
+abstract class MonthStorage extends EntityStorageAsync<int, WorkMonth> {
+  Future<bool> containsMonth(DateTime date);
+  Future updateOrCreateByDate(WorkMonth month);
+}
--- lib/storage/sembast/work_month_sembast_storage.dart
+++ lib/storage/sembast/work_month_sembast_storage.dart
@@ -1,6 +1,7 @@
 import 'package:open_work_flutter/data/models/work_month.dart';
 import 'package:open_work_flutter/storage/month_storage.dart';
 import 'package:open_work_flutter/storage/sembast/sembast_storage.dart';
+import 'package:sembast/sembast.dart';
 
 class WorkMonthSembastStorage extends SembastStorage<WorkMonth>
     implements MonthStorage {
@@ -15,5 +16,41 @@ class WorkMonthSembastStorage extends SembastStorage<WorkMonth>
   @override
   int? getId(WorkMonth entity) {
     return entity.id;
+  }
+
+  @override
+  Future<bool> containsMonth(DateTime date) async {
+    final id = await getIdByDate(date);
+
+    return id != null;
+  }
+
+  @override
+  Future updateOrCreateByDate(WorkMonth month) async {
+    final copy = month.copyWith(
+      id: await getIdByDate(month.date),
+    );
+
+    return updateOrCreate(copy);
+  }
+
+  Future<int?> getIdByDate(DateTime date) async {
+    final finder = Finder(
+      filter: Filter.matchesRegExp('date', regexFromYearMonth(date)),
+    );
+
+    final result = await store.findFirst(
+      database,
+      finder: finder,
+    );
+
+    return result?.key;
+  }
+
+  RegExp regexFromYearMonth(DateTime date) {
+    final source =
+        date.toIso8601String().substring(0, 7); // 1969-07-20 -> 1969-07
+
+    return RegExp('^$source');
   }
 }
