32e5bd0a7155f53f8ec1cbaef19afa85cb0e2f85
sasha <tanksasha77@gmail.com>
2024-11-14 21:31:19 +0200

    feat: override == operator and hashCode in WorkUnit

--- lib/data/models/work_unit.dart
+++ lib/data/models/work_unit.dart
@@ -2,4 +2,12 @@ class WorkUnit {
   final String value;
 
   WorkUnit(this.value);
+
+  @override
+  bool operator ==(Object other) {
+    return other is WorkUnit && other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
 }
